7070b9e3b70679ea84a6ff99401d22993b3050a8
minh quan <quan@opsify.dev>
2022-07-26 10:14:00 +0700

    inital

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "AWS region"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
+}
+
